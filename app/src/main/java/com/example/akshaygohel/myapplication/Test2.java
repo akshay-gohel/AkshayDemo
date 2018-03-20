fb52c5b4916da9dc0d0f208482553917a0845cbc
Akshay Gohel <akshay.gohel@nihilent.com>
2018-03-20 12:19:24 +0530

    test2 added
    made test2 appcompactactivity

---
+++ app/src/main/java/com/example/akshaygohel/myapplication/Test2.java
@@ -0,0 +1,13 @@
+package com.example.akshaygohel.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by akshay.gohel on 20-03-2018.
+ */
+
+public class Test2 extends AppCompatActivity{
+
+
+
+}
